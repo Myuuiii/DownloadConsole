d10756becfe9baecd651af8e22bc08e4c87325ab
Myuuiii <myu@subsilence.nl>
2021-10-01 18:13:44 +0200

    Multidownload Added

--- src/DownloadConsole/Program.cs
+++ src/DownloadConsole/Program.cs
@@ -16,7 +16,7 @@ namespace DownloadConsole
 		private static bool _stayOn = true;
 
 		private const string _downloadSingleOption = "Download Single URL";
-		private const string _downloadConfigOption = "[red]Download using stored configuration[/]";
+		private const string _downloadConfigOption = "Download using stored configuration";
 		private const string _reloadConfigOption = "Reload configuration";
 		private const string _customizeConfigurationOption = "[red]Edit configuration[/]";
 		private const string _exitOption = "Exit";
@@ -72,9 +72,13 @@ namespace DownloadConsole
 						DownloadSingleMenu();
 						break;
 					case _downloadConfigOption:
+						DownloadMultiMenu();
 						break;
 					case _reloadConfigOption:
 						_config = JsonSerializer.Deserialize<DCConfig>(File.ReadAllText("config.json"));
+						AnsiConsole.Clear();
+						AnsiConsole.MarkupLine("[yellow]Configuration was reloaded[/]");
+						Thread.Sleep(2000);
 						break;
 					case _customizeConfigurationOption:
 						break;
@@ -103,10 +107,77 @@ namespace DownloadConsole
 			return option;
 		}
 
+		static void DownloadMultiMenu()
+		{
+			AnsiConsole.Clear();
+			AnsiConsole.Render(new Rule("Downloading multiple URLs").Alignment(Justify.Left));
+			string[] dataLines = File.ReadAllLines(_config.SourcesFile);
+			int errors = 0;
+
+			foreach (string dataLine in dataLines)
+			{
+				AnsiConsole.Clear();
+				AnsiConsole.Render(new Rule("Downloading multiple URLs (In progress)").Alignment(Justify.Left));
+				string sourceUrl = dataLine.Split(' ')[0];
+				string targetFormat = dataLine.Split(' ')[1];
+
+				bool isRecognized = CheckUrlValidity(sourceUrl);
+				UrlSource source = DetectUrlSource(sourceUrl);
+				bool canDonwload = false;
+
+				if (source == UrlSource.Spotify || source == UrlSource.Soundcloud)
+				{
+					if (AudioFormats.Contains(targetFormat))
+					{ canDonwload = true; }
+				}
+				else if (source == UrlSource.YouTube)
+				{
+					if (AudioFormats.Contains(targetFormat) || VideoFormats.Contains(targetFormat))
+					{ canDonwload = true; }
+				}
+
+
+				if (source == UrlSource.NoSource || isRecognized == false)
+				{
+					AnsiConsole.WriteLine("The source of this URL wasn't recognized");
+					Thread.Sleep(1000);
+					continue;
+				}
+
+				AnsiConsole.MarkupLine($"Format: [aqua]{targetFormat}[/]");
+				ShowDownloadInformation();
+
+				// Start the download
+				AnsiConsole.WriteLine();
+				AnsiConsole.Render(new Rule("Download Output").Alignment(Justify.Left));
+				if (Download(source, sourceUrl, targetFormat))
+				{
+					AnsiConsole.MarkupLine($"[lime] Download Succesful [/]: {sourceUrl}");
+				}
+				else
+				{
+					AnsiConsole.MarkupLine($"[red] Download Failed [/]: {sourceUrl}");
+					errors++;
+				}
+				AnsiConsole.Render(new Rule("Download complete").Alignment(Justify.Left));
+			}
+
+			AnsiConsole.Clear();
+			AnsiConsole.Render(new Rule("Downloading multiple URLs (Completed)").Alignment(Justify.Left));
+
+			AnsiConsole.MarkupLine("[lime] Successfully completed queue [/]");
+
+			if (errors > 0)
+				AnsiConsole.MarkupLine($"In total, {errors} links could not be downloaded due to unknown problems");
+
+			AnsiConsole.WriteLine("Press any key to go back to the main menu");
+			Console.ReadKey();
+		}
+
 		static void DownloadSingleMenu()
 		{
 			AnsiConsole.Clear();
-			AnsiConsole.Render(new Rule("Downloading single URL").Alignment(Justify.Left));
+			AnsiConsole.Render(new Rule("Downloading single URL (In progress)").Alignment(Justify.Left));
 
 			string url = AnsiConsole.Ask<string>("Please enter the URL: ");
 
@@ -120,6 +191,55 @@ namespace DownloadConsole
 				return;
 			}
 
+			string format = SelectFormat(source);
+			AnsiConsole.MarkupLine($"Format: [aqua]{format}[/]");
+
+			ShowDownloadInformation();
+
+			// Start the download
+			if (Download(source, url, format))
+			{
+				AnsiConsole.Clear();
+				AnsiConsole.Render(new Rule("Downloading single URL (Completed)").Alignment(Justify.Left));
+				AnsiConsole.MarkupLine("[lime] Download Succesful [/]");
+				AnsiConsole.WriteLine("Press any key to return to the main menu...");
+			}
+			else
+			{
+				AnsiConsole.Clear();
+				AnsiConsole.Render(new Rule("Downloading single URL (Failed)").Alignment(Justify.Left));
+				AnsiConsole.MarkupLine("[red] Download Failed [/]");
+				AnsiConsole.WriteLine("Press any key to return to the main menu...");
+			}
+			Console.ReadKey();
+		}
+
+
+		//
+		//
+		//
+		// Functions
+		//
+		//
+		//
+
+		static void ShowDownloadInformation()
+		{
+			AnsiConsole.Markup($"Download Thumbail: ");
+			if (_config.DownloadThumbnails)
+				AnsiConsole.MarkupLine("[lime]Yes[/]");
+			else
+				AnsiConsole.MarkupLine("[red]No[/]");
+
+			AnsiConsole.Markup($"Attatch Thumbail: ");
+			if (_config.AttatchThumbnails)
+				AnsiConsole.MarkupLine("[lime]Yes[/]");
+			else
+				AnsiConsole.MarkupLine("[red]No[/]");
+		}
+
+		static string SelectFormat(UrlSource source)
+		{
 			string format = "";
 			SelectionPrompt<string> selectionPrompt = new SelectionPrompt<string>();
 			selectionPrompt.Title("Please select a format");
@@ -135,44 +255,10 @@ namespace DownloadConsole
 					selectionPrompt.AddChoices(AudioFormats);
 					break;
 			}
-
 			format = AnsiConsole.Prompt(selectionPrompt);
-			AnsiConsole.MarkupLine($"Format: [aqua]{format}[/]");
 
-			AnsiConsole.Markup($"Download Thumbail: ");
-			if (_config.DownloadThumbnails)
-				AnsiConsole.MarkupLine("[lime]Yes[/]");
-			else
-				AnsiConsole.MarkupLine("[red]No[/]");
-
-			AnsiConsole.Markup($"Attatch Thumbail: ");
-			if (_config.AttatchThumbnails)
-				AnsiConsole.MarkupLine("[lime]Yes[/]");
-			else
-				AnsiConsole.MarkupLine("[red]No[/]");
-
-			// Start the download
-			if (Download(source, url, format))
-			{
-				AnsiConsole.MarkupLine("[lime] Download Succesful [/]");
-				AnsiConsole.WriteLine("Press any key to return to the main menu...");
-			}
-			else
-			{
-				AnsiConsole.MarkupLine("[red] Download Failed [/]");
-				AnsiConsole.WriteLine("Press any key to return to the main menu...");
-			}
-			Console.ReadKey();
+			return format;
 		}
-
-
-		//
-		//
-		//
-		// Functions
-		//
-		//
-		//
 		static bool CheckUrlValidity(string url)
 		{
 			bool result = false;
@@ -249,7 +335,7 @@ namespace DownloadConsole
 						}
 
 						if (VideoFormats.Contains(format))
-							command.Append($"--format 'bestvideo+bestaudio' ");
+							command.Append($"--format \"bestvideo+bestaudio[ext=m4a]/bestvideo+bestaudio/best\" --merge-output-format {format} ");
 
 						command.Append(url);
 						break;
